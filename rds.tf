aa0ecaaa4566ccb2b429114b8d40790097970bb2
aaquibsayyad <121621186+aaquibsayyad@users.noreply.github.com>
2023-04-19 15:02:07 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -2,5 +2,29 @@ resource "aws_db_instance" "wordpress" {
 
   engine            = "mysql"
 
-  instance_class    = "db
+  instance_class    = "db.t4g.micro"
+
+  name              = var.db_name
+
+  username          = var.db_username
+
+  password          = var.db_password
+
+  allocated_storage = 20
+
+  vpc_security_group_ids = [aws_security_group.wordpress.id]
+
+  publicly_accessible = false
+
+  tags = {
+
+    Name = "${var.app_name}-db"
+
+  }
+
+}
+
+
+
+
 
